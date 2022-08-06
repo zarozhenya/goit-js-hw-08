2a7fba88482a393cfd83ae042eb4d8b6c3bf9649
Yevhen <eug.z2004@gmail.com>
2022-08-06 22:43:49 +0300

    task2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_VIDEO_TAG = 'videoplayer-current-time';
+
+const videoRef = document.querySelector('iframe');
+
+const videoPlayer = new Player(videoRef);
+
+const onTimeUpdate = ({ seconds }) => {
+  localStorage.setItem(LOCALSTORAGE_VIDEO_TAG, seconds);
+};
+
+videoPlayer.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+const populateTime = () => {
+  const seconds = localStorage.getItem(LOCALSTORAGE_VIDEO_TAG) || 0;
+  videoPlayer.setCurrentTime(seconds);
+};
+
+populateTime();
